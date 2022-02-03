753bb52194d7df574b5262ea5200b38e68867652
Andy Dupree <dupreeandyme@gmail.com>
2022-02-03 12:58:59 -0500

    finished naive

--- naive.py
+++ naive.py
@@ -10,7 +10,7 @@ from random import randrange
 
 # get the answers as a list
 ans_file = open("answers.txt")
-answers = ans_file.readlines()
+answers = [s[:-1] for s in ans_file.readlines()]
 ans_file.close()
 
 # give instruction text
@@ -21,15 +21,32 @@ print("+ -> character in right place")
 
 
 # loop until we've got an answer
-for i in range(1, 6):
+for i in range(1, 7):
     guess = answers[randrange(0, len(answers))]
-    print("guess {}: {}".format(i, guess))
+    if len(answers) > 10:
+        print("contains shard: {}".format(answers.count("shard") == 1))
+    print("guess {} (answers left: {}): {}".format(i, len(answers), guess))
     feedback = input("result: ")
-    if len(feedback) != 6:
-        print("error: length must be 6")
+    # error check
+    if len(feedback) != 5:
+        print("error: length must be 5")
         break
-    elif feedback.count("_") + feedback.count("*") + feedback.count("+") != 6:
+    elif feedback.count("_") + feedback.count("*") + feedback.count("+") != 5:
         print("error: must contain only _,+,*")
+    # filter out answers
+    for j in range(5):
+        char = guess[j]
+        expected = sum([1 if "+*".count(feedback[i]) != 0 and guess[i] == char else 0
+                        for i in range(5)])
+        if feedback[j] == "_":
+            answers = list(
+                filter(lambda s: s.count(char) == expected, answers))
+        elif feedback[j] == "*":
+            answers = list(filter(lambda s: s.count(char) == expected
+                                  and s[j] != char, answers))
+        elif feedback[j] == "+":
+            answers = list(filter(lambda s: s[j] == char, answers))
+
 # we're done... give an answer
 if len(answers) == 0:
     print("solution: {}".format(answers))
