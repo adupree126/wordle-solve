502d03ddce83dc19aed6cc09ada78ab23bb06725
Andy Dupree <dupreeandyme@gmail.com>
2022-02-03 11:53:44 -0500

    added error checking

--- naive.py
+++ naive.py
@@ -6,14 +6,32 @@ Algorithm as follows:
     - filter answer list
     - repeat
 """
+from random import randrange
+
 # get the answers as a list
 ans_file = open("answers.txt")
 answers = ans_file.readlines()
 ans_file.close()
 
-# loop until we've got an answer
-while len(answers) > 1:
-    pass
+# give instruction text
+print("key:")
+print("_ -> character not found")
+print("* -> character in wrong place")
+print("+ -> character in right place")
 
+
+# loop until we've got an answer
+for i in range(1, 6):
+    guess = answers[randrange(0, len(answers))]
+    print("guess {}: {}".format(i, guess))
+    feedback = input("result: ")
+    if len(feedback) != 6:
+        print("error: length must be 6")
+        break
+    elif feedback.count("_") + feedback.count("*") + feedback.count("+") != 6:
+        print("error: must contain only _,+,*")
 # we're done... give an answer
-print("solution: {}".format(answers))
+if len(answers) == 0:
+    print("solution: {}".format(answers))
+else:
+    print("failed with final candidates: {}".format(answers))
